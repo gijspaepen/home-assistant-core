home-assistant/core

Rework audit-licenses check [ci]

script/licenses.py
@@ -2,19 +2,35 @@
 
 from __future__ import annotations
 
+from argparse import ArgumentParser
+from collections.abc import Sequence
 from dataclasses import dataclass
+from enum import Flag, auto
 import json
+import logging
 from pathlib import Path
 import sys
 
 from awesomeversion import AwesomeVersion
+from license_expression import (
+    AND,
+    OR,
+    ExpressionError,
+    LicenseExpression,
+    LicenseSymbol,
+    get_spdx_licensing,
+)
+
+licensing = get_spdx_licensing()
+logger = logging.getLogger(__name__)
 
 
 @dataclass
 class PackageDefinition:
     """Package definition."""
 
-    license: str
+    license_classifier: list[str]
+    license_metadata: str
     name: str
     version: AwesomeVersion
 
@@ -22,13 +38,35 @@ class PackageDefinition:
     def from_dict(cls, data: dict[str, str]) -> PackageDefinition:
         """Create a package definition from a dictionary."""
         return cls(
-            license=data["License"],
+            license_classifier=data["License-Classifier"].split("; "),
+            license_metadata=data["License-Metadata"],
             name=data["Name"],
             version=AwesomeVersion(data["Version"]),
         )
 
 
-OSI_APPROVED_LICENSES = {
+# Incomplete list of OSI approved SPDX identifiers
+# Add more as needed
+OSI_APPROVED_LICENSES_SPDX = {
+    "Apache-2.0",
+    "BSD-3-Clause",
+    "BSD-2-Clause",
+    "GPL-2.0-only",
+    "GPL-2.0-or-later",
+    "GPL-3.0-only",
+    "GPL-3.0-or-later",
+    "HPND",
+    "ISC",
+    "LGPL-2.1-or-later",
+    "LGPL-3.0-only",
+    "LGPL-3.0-or-later",
+    "MIT",
+    "MPL-2.0",
+    "PSF-2.0",  # not approved
+    "Unlicense",
+}
+
+OSI_APPROVED_LICENSE_CLASSIFIER = {
     "Academic Free License (AFL)",
     "Apache Software License",
     "Apple Public Source License",
@@ -96,26 +134,6 @@ OSI_APPROVED_LICENSES = {
     "Zero-Clause BSD (0BSD)",
     "Zope Public License",
     "zlib/libpng License",
-    "Apache License",
-    "MIT",
-    "apache-2.0",
-    "GPL-3.0",
-    "GPLv3+",
-    "MPL2",
-    "MPL-2.0",
-    "Apache 2",
-    "LGPL v3",
-    "BSD",
-    "GNU-3.0",
-    "GPLv3",
-    "Eclipse Public License v2.0",
-    "ISC",
-    "GPL-2.0-only",
-    "mit",
-    "GNU General Public License v3",
-    "Unlicense",
-    "Apache-2",
-    "GPLv2",
 }
 
 EXCEPTIONS = {
@@ -132,7 +150,6 @@ EXCEPTIONS = {
     "apple_weatherkit",  # https://github.com/tjhorner/python-weatherkit/pull/3
     "asyncio",  # PSF License
     "chacha20poly1305",  # LGPL
-    "chacha20poly1305-reuseable",  # Apache 2.0 or BSD 3-Clause
     "commentjson",  # https://github.com/vaidik/commentjson/pull/55
     "crownstone-cloud",  # https://github.com/crownstone/crownstone-lib-python-cloud/pull/5
     "crownstone-core",  # https://github.com/crownstone/crownstone-lib-python-core/pull/6
@@ -171,7 +188,6 @@ EXCEPTIONS = {
     "tapsaff",  # https://github.com/bazwilliams/python-taps-aff/pull/5
     "tellduslive",  # https://github.com/molobrakos/tellduslive/pull/24
     "tellsticknet",  # https://github.com/molobrakos/tellsticknet/pull/33
-    "vincenty",  # Public domain
     "zeversolar",  # https://github.com/kvanzuijlen/zeversolar/pull/46
 }
 
@@ -189,61 +205,250 @@ TODO = {
     "uvcclient": AwesomeVersion(
         "0.11.0"
     ),  # No License https://github.com/kk7ds/uvcclient/issues/7
+    # -- Full license text in metadata
+    "PyNINA": AwesomeVersion("0.3.3"),  # MIT
+    "dicttoxml": AwesomeVersion("1.7.16"),  # GPL
+    "homematicip": AwesomeVersion("1.1.1"),  # GPL
+    "ibmiotf": AwesomeVersion("0.3.4"),  # Eclipse Public License
+    "xknxproject": AwesomeVersion("3.7.1"),  # GPL
+    "zwave-js-server-python": AwesomeVersion("0.57.0"),  # Apache
+    # -- Not SPDX license strings --
+    "PyNaCl": AwesomeVersion("1.5.0"),  # Apache License 2.0
+    "PySocks": AwesomeVersion("1.7.1"),  # BSD
+    "aioairq": AwesomeVersion("0.3.2"),  # Apache License, Version 2.0
+    "aioaquacell": AwesomeVersion("0.2.0"),  # Apache License 2.0
+    "aioconsole": AwesomeVersion("0.7.1"),  # GPLv3
+    "aioeagle": AwesomeVersion("1.1.0"),  # Apache License 2.0
+    "aiohttp-socks": AwesomeVersion("0.8.4"),  # Apache 2
+    "aiolivisi": AwesomeVersion("0.0.19"),  # Apache License 2.0
+    "aiopegelonline": AwesomeVersion("0.0.10"),  # Apache License 2.0
+    "aioshelly": AwesomeVersion("11.1.0"),  # Apache License 2.0
+    "aiovlc": AwesomeVersion("0.3.2"),  # Apache Software License 2.0
+    "amcrest": AwesomeVersion("1.9.8"),  # GPLv2
+    "async-modbus": AwesomeVersion("0.2.1"),  # GNU General Public License v3
+    "asyncssh": AwesomeVersion("2.15.0"),  # Eclipse Public License v2.0
+    "baidu-aip": AwesomeVersion("1.6.6.0"),  # Apache License
+    "bimmer_connected": AwesomeVersion("0.16.1"),  # Apache-2
+    "bs4": AwesomeVersion("0.0.2"),  # MIT License
+    "bt-proximity": AwesomeVersion("0.2.1"),  # Apache 2.0
+    "connio": AwesomeVersion("0.2.0"),  # GPLv3+
+    "datapoint": AwesomeVersion("0.9.9"),  # GPLv3
+    "electrickiwi-api": AwesomeVersion("0.8.5"),  # GNU-3.0
+    "freebox-api": AwesomeVersion("1.1.0"),  # GNU GPL v3
+    "gpiod": AwesomeVersion("2.2.1"),  # LGPLv2.1
+    "insteon-frontend-home-assistant": AwesomeVersion("0.5.0"),  # MIT License
+    "jwcrypto": AwesomeVersion("1.5.6"),  # LGPLv3+
+    "knx_frontend": AwesomeVersion("2024.7.25.204106"),  # MIT License
+    "libpyfoscam": AwesomeVersion("1.2.2"),  # LGPLv3+
+    "london-tube-status": AwesomeVersion("0.5"),  # Apache License, Version 2.0
+    "mutesync": AwesomeVersion("0.0.1"),  # Apache License 2.0
+    "oemthermostat": AwesomeVersion("1.1.1"),  # BSD
+    "paho-mqtt": AwesomeVersion(
+        "1.6.1"
+    ),  # Eclipse Public License v2.0 / Eclipse Distribution License v1.0
+    "passlib": AwesomeVersion("1.7.4"),  # BSD
+    "pilight": AwesomeVersion("0.1.1"),  # MIT License
+    "ply": AwesomeVersion("3.11"),  # BSD
+    "protobuf": AwesomeVersion("4.25.1"),  # 3-Clause BSD License
+    "psutil-home-assistant": AwesomeVersion("0.0.1"),  # Apache License 2.0
+    "pure-pcapy3": AwesomeVersion("1.0.1"),  # Simplified BSD
+    "py-vapid": AwesomeVersion("1.9.1"),  # MPL2
+    "pyAtome": AwesomeVersion("0.1.1"),  # Apache Software License
+    "pybotvac": AwesomeVersion("0.0.25"),  # Licensed under the MIT license
+    "pychannels": AwesomeVersion("1.2.3"),  # The MIT License
+    "pycognito": AwesomeVersion("2024.5.1"),  # Apache License 2.0
+    "pycountry": AwesomeVersion("23.12.11"),  # LGPL 2.1
+    "pycryptodome": AwesomeVersion("3.20.0"),  # BSD, Public Domain
+    "pycryptodomex": AwesomeVersion("3.20.0"),  # BSD, Public Domain
+    "pydanfossair": AwesomeVersion("0.1.0"),  # Apache 2.0
+    "pydrawise": AwesomeVersion("2024.6.4"),  # Apache License 2.0
+    "pydroid-ipcam": AwesomeVersion("2.0.0"),  # Apache License 2.0
+    "pyebox": AwesomeVersion("1.1.4"),  # Apache 2.0
+    "pyevilgenius": AwesomeVersion("2.0.0"),  # Apache License 2.0
+    "pyezviz": AwesomeVersion("0.2.1.2"),  # Apache Software License 2.0
+    "pyfido": AwesomeVersion("2.1.2"),  # Apache 2.0
+    "pyialarm": AwesomeVersion("2.2.0"),  # Apache 2.0
+    "pylitejet": AwesomeVersion("0.6.2"),  # MIT License
+    "pyquery": AwesomeVersion("2.0.0"),  # BSD
+    "pyschlage": AwesomeVersion("2024.6.0"),  # Apache License 2.0
+    "pysuez": AwesomeVersion("0.2.0"),  # Apache 2.0
+    "python-digitalocean": AwesomeVersion("1.13.2"),  # LGPL v3
+    "python-socks": AwesomeVersion("2.5.0"),  # Apache 2
+    "pywebpush": AwesomeVersion("1.14.1"),  # MPL2
+    "raincloudy": AwesomeVersion("0.0.7"),  # Apache License 2.0
+    "securetar": AwesomeVersion("2024.2.1"),  # Apache License 2.0
+    "simplehound": AwesomeVersion("0.3"),  # Apache License, Version 2.0
+    "sockio": AwesomeVersion("0.15.0"),  # GPLv3+
+    "starkbank-ecdsa": AwesomeVersion("2.2.0"),  # MIT License
+    "streamlabswater": AwesomeVersion("1.0.1"),  # Apache 2.0
+    "vilfo-api-client": AwesomeVersion("0.5.0"),  # MIT License
+    "voluptuous-openapi": AwesomeVersion("0.0.5"),  # Apache License 2.0
+    "voluptuous-serialize": AwesomeVersion("2.6.0"),  # Apache License 2.0
+    "vultr": AwesomeVersion("0.1.2"),  # The MIT License (MIT)
+    "wallbox": AwesomeVersion("0.7.0"),  # Apache 2
+    "zeroconf": AwesomeVersion("0.132.2"),  # LGPL
+    "zha-quirks": AwesomeVersion("0.0.117"),  # Apache License Version 2.0
+    "zhong-hong-hvac": AwesomeVersion("1.0.12"),  # Apache
 }
 
+EXCEPTIONS_AND_TODOS = EXCEPTIONS.union(TODO.keys())
 
-def main() -> int:
+
+def check_spdx_license(expr: LicenseExpression) -> bool:
+    """Check a spdx license expression."""
+    if isinstance(expr, LicenseSymbol):
+        return expr.key in OSI_APPROVED_LICENSES_SPDX
+    if isinstance(expr, OR):
+        return any(check_spdx_license(arg) for arg in expr.args)
+    if isinstance(expr, AND):
+        return all(check_spdx_license(arg) for arg in expr.args)
+    return False
+
+
+def check_license_metadata(license: str, package_name: str) -> bool | None:
+    """Check if license metadata is a valid and approved SPDX license string."""
+    if license == "UNKNOWN" or "\n" in license:
+        # Ignore mulitline license strings
+        # Those are often full license texts which discurraged
+        return None
+    try:
+        expr = licensing.parse(license, validate=True)
+    except ExpressionError:
+        logger.debug(
+            "Not a validate metadata license for %s: %s",
+            package_name,
+            license,
+        )
+        return None
+    return check_spdx_license(expr)
+
+
+def check_license_classifier(licenses: list[str], package_name: str) -> bool | None:
+    """Check license classifier are OSI approved."""
+    assert len(licenses) > 0
+    if licenses[0] == "UNKNOWN":
+        return None
+    if len(licenses) > 1:
+        # It's not defined how multiple license classifier should be interpreted
+        # To be safe required ALL to be approved
+        check = all(
+            classifier in OSI_APPROVED_LICENSE_CLASSIFIER for classifier in licenses
+        )
+        if check is False:
+            logger.debug(
+                "Not all classifier approved for %s: %s", package_name, licenses
+            )
+        return check
+    return licenses[0] in OSI_APPROVED_LICENSE_CLASSIFIER
+
+
+class Status(Flag):
+    """License status flag."""
+
+    APPROVED = auto()
+    NOT_APPROVED = auto()
+    METADATA = auto()
+    CLASSIFIER = auto()
+    UNKNOWN = auto()
+
+
+def determine_license_status(package: PackageDefinition) -> Status:
+    """Determine license status.
+
+    First, check license metadata then license classifier.
+    """
+    check = check_license_metadata(package.license_metadata, package.name)
+    if check is True:
+        return Status.APPROVED | Status.METADATA
+    if check is False:
+        return Status.NOT_APPROVED | Status.METADATA
+
+    check = check_license_classifier(package.license_classifier, package.name)
+    if check is True:
+        return Status.APPROVED | Status.CLASSIFIER
+    if check is False:
+        return Status.NOT_APPROVED | Status.CLASSIFIER
+
+    return Status.NOT_APPROVED | Status.UNKNOWN
+
+
+def get_license_str(pkg: PackageDefinition, status: Status) -> str:
+    """Return combined license string."""
+    return f"{pkg.license_metadata} -- {pkg.license_classifier}"
+
+
+def main(argv: Sequence[str] | None = None) -> int:
     """Run the main script."""
-    raw_licenses = json.loads(Path("licenses.json").read_text())
-    package_definitions = [PackageDefinition.from_dict(data) for data in raw_licenses]
     exit_code = 0
-    for package in package_definitions:
-        previous_unapproved_version = TODO.get(package.name)
-        approved = False
-        for approved_license in OSI_APPROVED_LICENSES:
-            if approved_license in package.license:
-                approved = True
-                break
-        if previous_unapproved_version is not None:
-            if previous_unapproved_version < package.version:
-                if approved:
-                    print(
-                        "Approved license detected for"
-                        f"{package.name}@{package.version}: {package.license}"
-                    )
-                    print("Please remove the package from the TODO list.")
-                    print()
-                else:
-                    print(
-                        "We could not detect an OSI-approved license for "
-                        f"{package.name}@{package.version}: {package.license}"
-                    )
-                    print()
-                exit_code = 1
-        elif not approved and package.name not in EXCEPTIONS:
+    logging.basicConfig(
+        format="%(levelname)s:%(message)s", stream=sys.stdout, level=logging.INFO
+    )
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "path",
+        nargs="?",
+        metavar="PATH",
+        default="licenses.json",
+        help="Path to json licenses file",
+    )
+    parser.add_argument(
+        "-v", dest="verbose", action="store_true", help="Enable verbose logging"
+    )
+
+    argv = argv or sys.argv[1:]
+    args = parser.parse_args(argv)
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    raw_licenses = json.loads(Path(args.path).read_text())
+    license_status: dict[str, tuple[PackageDefinition, Status]] = {
+        package.name: (package, determine_license_status(package))
+        for data in raw_licenses
+        if (package := PackageDefinition.from_dict(data))
+    }
+
+    for package_name, version in TODO.items():
+        pkg, status = license_status.get(package_name, (None, None))
+        if pkg is None or not (version < pkg.version):
+            continue
+        assert status
+        if Status.APPROVED in status:
             print(
-                "We could not detect an OSI-approved license for"
-                f"{package.name}@{package.version}: {package.license}"
+                "Approved license detected for "
+                f"{pkg.name}@{pkg.version}: {get_license_str(pkg, status)}\n"
+                "Please remove the package from the TODO list.\n"
             )
-            print()
-            exit_code = 1
-        elif approved and package.name in EXCEPTIONS:
+        else:
             print(
-                "Approved license detected for"
-                f"{package.name}@{package.version}: {package.license}"
+                "We could not detect an OSI-approved license for "
+                f"{pkg.name}@{pkg.version}: {get_license_str(pkg, status)}\n"
+                "Please update the package version on the TODO list.\n"
             )
-            print(f"Please remove the package from the EXCEPTIONS list: {package.name}")
-            print()
-            exit_code = 1
-    current_packages = {package.name for package in package_definitions}
-    for package in [*TODO.keys(), *EXCEPTIONS]:
-        if package not in current_packages:
+        exit_code = 1
+
+    for pkg, status in license_status.values():
+        if Status.NOT_APPROVED in status and pkg.name not in EXCEPTIONS_AND_TODOS:
             print(
-                f"Package {package} is tracked, but not used. Please remove from the licenses.py"
-                "file."
+                "We could not detect an OSI-approved license for "
+                f"{pkg.name}@{pkg.version}: {get_license_str(pkg, status)}\n"
             )
-            print()
             exit_code = 1
+        elif Status.APPROVED in status and pkg.name in EXCEPTIONS_AND_TODOS:
+            print(
+                "Approved license detected for "
+                f"{pkg.name}@{pkg.version}: {get_license_str(pkg, status)}\n"
+                f"Please remove the package from the EXCEPTIONS list: {pkg.name}\n"
+            )
+            exit_code = 1
+
+    for package_name in EXCEPTIONS_AND_TODOS.difference(license_status.keys()):
+        print(
+            f"Package {package_name} is tracked, but not used. Please remove from the licenses.py"
+            "file.\n"
+        )
+        exit_code = 1
+
     return exit_code
 
 

gijspaepen/home-assistant-core

script/licenses.py
@@ -7,6 +7,7 @@ from collections.abc import Sequence
 from dataclasses import dataclass
 from importlib import metadata
 import json
+import logging
 from pathlib import Path
 import sys
 from typing import TypedDict, cast
@@ -22,6 +23,7 @@ from license_expression import (
 )
 
 licensing = get_spdx_licensing()
+logger = logging.getLogger(__name__)
 
 
 class PackageMetadata(TypedDict):
@@ -38,7 +40,6 @@ class PackageMetadata(TypedDict):
 class PackageDefinition:
     """Package definition."""
 
-    license: str
     license_expression: str | None
     license_metadata: str | None
     license_classifier: list[str]
@@ -48,10 +49,7 @@ class PackageDefinition:
     @classmethod
     def from_dict(cls, data: PackageMetadata) -> PackageDefinition:
         """Create a package definition from PackageMetadata."""
-        if not (license_str := "; ".join(data["license_classifier"])):
-            license_str = data["license_metadata"] or "UNKNOWN"
         return cls(
-            license=license_str,
             license_expression=data["license_expression"],
             license_metadata=data["license_metadata"],
             license_classifier=data["license_classifier"],
@@ -93,7 +91,7 @@ OSI_APPROVED_LICENSES_SPDX = {
     "ZPL-2.1",
 }
 
-OSI_APPROVED_LICENSES = {
+OSI_APPROVED_LICENSE_CLASSIFIER = {
     "Academic Free License (AFL)",
     "Apache Software License",
     "Apple Public Source License",
@@ -161,19 +159,6 @@ OSI_APPROVED_LICENSES = {
     "Zero-Clause BSD (0BSD)",
     "Zope Public License",
     "zlib/libpng License",
-    # End license classifier
-    "Apache License",
-    "MIT",
-    "MPL2",
-    "Apache 2",
-    "LGPL v3",
-    "BSD",
-    "GNU-3.0",
-    "GPLv3",
-    "Eclipse Public License v2.0",
-    "ISC",
-    "GNU General Public License v3",
-    "GPLv2",
 }
 
 EXCEPTIONS = {
@@ -211,6 +196,84 @@ TODO = {
     "aiocache": AwesomeVersion(
         "0.12.3"
     ),  # https://github.com/aio-libs/aiocache/blob/master/LICENSE all rights reserved?
+    # -- Full license text in metadata
+    "PyNINA": AwesomeVersion("0.3.4"),  # MIT
+    "aioconsole": AwesomeVersion("0.8.1"),  # GPL
+    "dicttoxml": AwesomeVersion("1.7.16"),  # GPL
+    "homematicip": AwesomeVersion("1.1.3"),  # GPL
+    "ibmiotf": AwesomeVersion("0.3.4"),  # Eclipse Public License
+    "matrix-nio": AwesomeVersion("0.25.2"),  # ISC
+    "zwave-js-server-python": AwesomeVersion(
+        "0.59.1"
+    ),  # Apache  # https://github.com/home-assistant-libs/zwave-js-server-python/pull/1029
+    # -- Not SPDX license strings --
+    "PyNaCl": AwesomeVersion("1.5.0"),  # Apache License 2.0
+    "PySocks": AwesomeVersion("1.7.1"),  # BSD
+    "aioairq": AwesomeVersion("0.4.3"),  # Apache License, Version 2.0
+    "aioaquacell": AwesomeVersion("0.2.0"),  # Apache License 2.0
+    "aioeagle": AwesomeVersion("1.1.0"),  # Apache License 2.0
+    "aiolivisi": AwesomeVersion("0.0.19"),  # Apache License 2.0
+    "aiopegelonline": AwesomeVersion("0.0.10"),  # Apache License 2.0
+    "aioshelly": AwesomeVersion("12.0.1"),  # Apache License 2.0
+    "amcrest": AwesomeVersion("1.9.8"),  # GPLv2
+    "async-modbus": AwesomeVersion("0.2.1"),  # GNU General Public License v3
+    "asyncssh": AwesomeVersion("2.18.0"),  # Eclipse Public License v2.0
+    "baidu-aip": AwesomeVersion("1.6.6.0"),  # Apache License
+    "bs4": AwesomeVersion("0.0.2"),  # MIT License
+    "bt-proximity": AwesomeVersion("0.2.1"),  # Apache 2.0
+    "connio": AwesomeVersion("0.2.0"),  # GPLv3+
+    "datapoint": AwesomeVersion("0.9.9"),  # GPLv3
+    "electrickiwi-api": AwesomeVersion("0.8.5"),  # GNU-3.0
+    "freebox-api": AwesomeVersion("1.1.0"),  # GNU GPL v3
+    "gpiod": AwesomeVersion("2.2.2"),  # LGPLv2.1
+    "insteon-frontend-home-assistant": AwesomeVersion("0.5.0"),  # MIT License
+    "knx_frontend": AwesomeVersion("2024.11.16.205004"),  # MIT License
+    "lcn-frontend": AwesomeVersion("0.2.2"),  # MIT License
+    "libpyfoscam": AwesomeVersion("1.2.2"),  # LGPLv3+
+    "london-tube-status": AwesomeVersion("0.5"),  # Apache License, Version 2.0
+    "mutesync": AwesomeVersion("0.0.1"),  # Apache License 2.0
+    "oemthermostat": AwesomeVersion("1.1.1"),  # BSD
+    "paho-mqtt": AwesomeVersion(
+        "1.6.1"
+    ),  # Eclipse Public License v2.0 / Eclipse Distribution License v1.0
+    "pilight": AwesomeVersion("0.1.1"),  # MIT License
+    "ply": AwesomeVersion("3.11"),  # BSD
+    "protobuf": AwesomeVersion("5.28.3"),  # 3-Clause BSD License
+    "psutil-home-assistant": AwesomeVersion("0.0.1"),  # Apache License 2.0
+    "pure-pcapy3": AwesomeVersion("1.0.1"),  # Simplified BSD
+    "pyAtome": AwesomeVersion("0.1.1"),  # Apache Software License
+    "pybotvac": AwesomeVersion("0.0.25"),  # Licensed under the MIT license
+    "pychannels": AwesomeVersion("1.2.3"),  # The MIT License
+    "pycognito": AwesomeVersion("2024.5.1"),  # Apache License 2.0
+    "pycryptodome": AwesomeVersion("3.21.0"),  # BSD, Public Domain
+    "pycryptodomex": AwesomeVersion("3.21.0"),  # BSD, Public Domain
+    "pydanfossair": AwesomeVersion("0.1.0"),  # Apache 2.0
+    "pydrawise": AwesomeVersion("2024.9.0"),  # Apache License 2.0
+    "pydroid-ipcam": AwesomeVersion("2.0.0"),  # Apache License 2.0
+    "pyebox": AwesomeVersion("1.1.4"),  # Apache 2.0
+    "pyevilgenius": AwesomeVersion("2.0.0"),  # Apache License 2.0
+    "pyezviz": AwesomeVersion("0.2.1.2"),  # Apache Software License 2.0
+    "pyfido": AwesomeVersion("2.1.2"),  # Apache 2.0
+    "pyialarm": AwesomeVersion("2.2.0"),  # Apache 2.0
+    "pylitejet": AwesomeVersion("0.6.3"),  # MIT License
+    "pyquery": AwesomeVersion("2.0.1"),  # BSD
+    "pyschlage": AwesomeVersion("2024.8.0"),  # Apache License 2.0
+    "pysuezV2": AwesomeVersion("1.3.2"),  # Apache 2.0
+    "python-digitalocean": AwesomeVersion("1.13.2"),  # LGPL v3
+    "python-socks": AwesomeVersion("2.5.3"),  # Apache 2
+    "pywebpush": AwesomeVersion("1.14.1"),  # MPL2
+    "raincloudy": AwesomeVersion("0.0.7"),  # Apache License 2.0
+    "simplehound": AwesomeVersion("0.3"),  # Apache License, Version 2.0
+    "sockio": AwesomeVersion("0.15.0"),  # GPLv3+
+    "starkbank-ecdsa": AwesomeVersion("2.2.0"),  # MIT License
+    "streamlabswater": AwesomeVersion("1.0.1"),  # Apache 2.0
+    "vilfo-api-client": AwesomeVersion("0.5.0"),  # MIT License
+    "voluptuous-openapi": AwesomeVersion("0.0.5"),  # Apache License 2.0
+    "voluptuous-serialize": AwesomeVersion("2.6.0"),  # Apache License 2.0
+    "vultr": AwesomeVersion("0.1.2"),  # The MIT License (MIT)
+    "wallbox": AwesomeVersion("0.7.0"),  # Apache 2
+    "zha-quirks": AwesomeVersion("0.0.124"),  # Apache License Version 2.0
+    "zhong-hong-hvac": AwesomeVersion("1.0.13"),  # Apache
 }
 
 EXCEPTIONS_AND_TODOS = EXCEPTIONS.union(TODO)
@@ -275,22 +338,25 @@ def check_license_status(package: PackageDefinition) -> bool:
     """Check if package licenses is OSI approved."""
     if package.license_expression:
         # Prefer 'License-Expression' if it exists
-        return check_license_expression(package.license_expression) or False
+        return (
+            check_license_expression(package.license_expression, package.name) or False
+        )
 
     if (
         package.license_metadata
-        and (check := check_license_expression(package.license_metadata)) is not None
+        and (check := check_license_expression(package.license_metadata, package.name))
+        is not None
     ):
         # Check license metadata if it's a valid SPDX license expression
         return check
 
-    for approved_license in OSI_APPROVED_LICENSES:
-        if approved_license in package.license:
-            return True
+    if check := check_license_classifier(package.license_classifier, package.name):
+        return check
+
     return False
 
 
-def check_license_expression(license_str: str) -> bool | None:
+def check_license_expression(license_str: str, package_name: str) -> bool | None:
     """Check if license expression is a valid and approved SPDX license string."""
     if license_str == "UNKNOWN" or "\n" in license_str:
         # Ignore common errors for license metadata values
@@ -299,6 +365,11 @@ def check_license_expression(license_str: str) -> bool | None:
     try:
         expr = licensing.parse(license_str, validate=True)
     except ExpressionError:
+        logger.debug(
+            "Not a validate metadata license for %s: %s",
+            package_name,
+            license_str,
+        )
         return None
     return check_spdx_license(expr)
 
@@ -312,6 +383,24 @@ def check_spdx_license(expr: LicenseExpression) -> bool:
     if isinstance(expr, AND):
         return all(check_spdx_license(arg) for arg in expr.args)
     return False
+
+
+def check_license_classifier(licenses: list[str], package_name: str) -> bool | None:
+    """Check license classifier are OSI approved."""
+    if not licenses:
+        return None
+    if len(licenses) > 1:
+        # It's not defined how multiple license classifier should be interpreted
+        # To be safe required ALL to be approved
+        check = all(
+            classifier in OSI_APPROVED_LICENSE_CLASSIFIER for classifier in licenses
+        )
+        if check is False:
+            logger.debug(
+                "Not all classifier approved for %s: %s", package_name, licenses
+            )
+        return check
+    return licenses[0] in OSI_APPROVED_LICENSE_CLASSIFIER
 
 
 def get_license_str(package: PackageDefinition) -> str:
@@ -375,6 +464,9 @@ class CheckArgs(Namespace):
 def main(argv: Sequence[str] | None = None) -> int:
     """Run the main script."""
     parser = ArgumentParser()
+    parser.add_argument(
+        "-v", dest="verbose", action="store_true", help="Enable verbose logging"
+    )
     subparsers = parser.add_subparsers(title="Subcommands", required=True)
 
     parser_extract = subparsers.add_parser("extract")
@@ -397,6 +489,12 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     argv = argv or sys.argv[1:]
     args = parser.parse_args(argv)
+
+    logging.basicConfig(
+        format="%(levelname)s:%(message)s", stream=sys.stdout, level=logging.INFO
+    )
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
     if args.action == "extract":
         args = cast(ExtractArgs, args)